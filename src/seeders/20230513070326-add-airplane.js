67848f85759b1cf02f947f5ed59a251f3494fffb
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-05-27 12:05:27 +0530

    seeded seats

--- src/seeders/20230513070326-add-airplane.js
+++ src/seeders/20230513070326-add-airplane.js
@@ -3,15 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
     await queryInterface.bulkInsert("Airplanes", [
       {
         modelNumber: "SD101",
@@ -35,12 +26,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     await queryInterface.bulkDelete('Airplanes', {});
   }
 };
